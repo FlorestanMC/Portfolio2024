e03eb758407247d4c91311615029ddf1478e4efd
Florestan Mellé-Cappellin <150163972+FlorestanMC@users.noreply.github.com>
2024-06-17 16:27:10 +0200

    fin de journée

--- app/components/landingblock/Landingblock.jsx
+++ app/components/landingblock/Landingblock.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import Link from 'next/link'
 
 export default function Landingblock() {
   return (
     <div>
         <div>
-            <div>
-                <h1 className='font-bold text-[156px]'>
-                    Florestan
-                </h1>
+            <div className='flex-col'>
+                <div>
+                    <h1 className='font-bold text-[156px]'>
+                        Florestan
+                    </h1>
+                </div>
                 <div>
                     <div>
                         <h3>Student in Computer Sciences & Web Developpement</h3>
@@ -35,17 +38,21 @@ export default function Landingblock() {
                 <img src="" alt="" />
             </div>
         </div>
-        <div>
+        <div className='flex'>
             <div>
 
             </div>
-            <div>
-                <h3></h3>
-                <div></div>
+            <div className='flex-col'>
+                <h3>SAE301 tilde MMI Storage</h3>
+                <div className='border border-[#FDC93F]'></div>
                 <div>
                     <div>
-                        <p></p>
-                        <div></div>
+                        <p>Une application permettant à un utilisateur de se créer un compte puis de pouvoir poster et présenter des projets. </p>
+                        <Link href="/">
+                            <div className='rounded-full w-4 h-4 border border-black'>
+                                <img src="" alt="" />
+                            </div>
+                        </Link>
                     </div>
                     <div>
                         <img src="" alt="" />
